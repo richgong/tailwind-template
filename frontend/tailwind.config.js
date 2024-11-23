8ac2dd2bbda708e21745a0e10707256c211e28c0
richard.gong <richard.gong@robinhood.com>
2024-11-22 17:12:32 -0800

    super clutch fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -16,7 +16,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+  content: ["src/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
   theme: {
     extend: {
       colors: {
